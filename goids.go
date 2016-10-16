cd67092eb04efb0888b0b37b00c8b4f5b7b848a4
Travis Cline <travis.cline@gmail.com>
2016-10-16 11:15:01 -0700

    swap files

--- goids.go
+++ goids.go
@@ -1,8 +1,60 @@
 package goids
 
-import "github.com/go-gl/mathgl/mgl32"
+import (
+	"math"
+	"math/rand"
+	"time"
 
-var (
-	ColorGreen = mgl32.Vec4{0.1, 0.8, 0.1, 1}
-	ColorBlue  = mgl32.Vec4{0.1, 0.1, 0.8, 1}
+	"github.com/go-gl/mathgl/mgl32"
 )
+
+type World struct {
+	Goids []*Goid
+}
+
+func (w *World) Step(dt time.Duration) {
+	for _, g := range w.Goids {
+		g.Step(dt)
+	}
+	// Separation
+	// Velocity matching
+	// Cohesion
+}
+
+type Goid struct {
+	X       float32
+	Y       float32
+	Heading float32 // in degrees
+	Speed   float32
+	Color   mgl32.Vec4
+}
+
+func NewGoid(speed float32) *Goid {
+	color := ColorGreen
+	if rand.NormFloat64() > 0.5 {
+		color = ColorBlue
+	}
+	return &Goid{
+		X:       0,
+		Y:       0,
+		Heading: float32(rand.Intn(360)),
+		Speed:   speed,
+		Color:   color,
+	}
+}
+
+func (g *Goid) Velocity() mgl32.Vec2 {
+	velocity := mgl32.Vec2{}
+	headingRad := float64(mgl32.DegToRad(g.Heading))
+	// correct for '0' meaning Y+
+	//headingRad += math.Pi
+	velocity[0] = -1 * float32(math.Sin(headingRad)) * g.Speed
+	velocity[1] = float32(math.Cos(headingRad)) * g.Speed
+	return velocity
+}
+
+func (g *Goid) Step(t time.Duration) {
+	v := g.Velocity()
+	g.X += v.X()
+	g.Y += v.Y()
+}
